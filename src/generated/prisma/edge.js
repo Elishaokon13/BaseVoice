7553a3cef4b16709481997cb6ded7c2b0be46c2d
Elishaokon13 <eliscodavid@gmail.com>
2025-06-01 03:04:54 +0100

    Modified src/generated/prisma/edge.js

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -124,7 +124,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/test/anon-feedback/src/generated/prisma",
+      "value": "/Users/test/BaseVoice/src/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -138,12 +138,11 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/test/anon-feedback/prisma/schema.prisma",
+    "sourceFilePath": "/Users/test/BaseVoice/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.8.2",
@@ -152,7 +151,7 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
+  "postinstall": true,
   "inlineDatasources": {
     "db": {
       "url": {
